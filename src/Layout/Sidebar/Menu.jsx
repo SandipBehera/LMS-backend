e826bb39b046e7e6629b601600b8a497cfd4a2b9
SandipBehera <sandip.sudip36@gmail.com>
2024-01-30 11:05:41 +0530

    first commit

---
+++ src/Layout/Sidebar/Menu.jsx
@@ -0,0 +1,16 @@
+export const EmployeeMenu = [
+  {
+    menutitle: "General",
+    menucontent: "Dashboards,Widgets",
+    Items: [
+      {
+        path: `/dashboard`,
+        icon: "home",
+        title: "Dashboard",
+        type: "link",
+        badge: "badge badge-light-primary",
+        active: false,
+      },
+    ],
+  },
+];
