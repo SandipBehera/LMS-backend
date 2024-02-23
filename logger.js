89325692a6dfbd9dfe527f2c662c42b39b7844d8
Abhishek Gupta <abskgupta11@gmail.com>
2024-02-23 15:01:21 +0530

    Logger file updated

--- logger.js
+++ logger.js
@@ -2,7 +2,7 @@ const pino = require("pino");
 const fs = require("fs");
 
 // Create a writable stream for the log file
-const date = new Date();
+const date = new Date().toTimeString().split(" ")[0].replaceAll(":","_");
 const logFileStream = fs.createWriteStream(`logs/${date}.log`, { flags: "a" });
 
 // Create a Pino logger with the file stream
